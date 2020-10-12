ff8050a0476211cc40538f8e7a5dbb8375405914
Tatiana Shved <tanya.shved.89@gmail.com>
2020-10-12 18:35:51 +0300

    вносит разметку

---
+++ menu.js
@@ -0,0 +1,15 @@
+import menuElement from './menu.json';
+import menuTemplate from '.templates/menu.hbs';
+import './theme.js';
+import './styles.css';
+
+let menu = document.querySelector('.js-menu');
+
+console.log(menu);
+
+function createMenuCards(array) {
+  const markup = array.map(post => menuTemplate(post)).join('');
+  menu.insertAdjacentHTML('beforeend', markup);
+}
+
+createMenuCards(menuElement);
