a863d1a50aa97963ae88c6d3cbbf225f75411bcf
Tatiana Shved <tanya.shved.89@gmail.com>
2020-10-15 13:25:20 +0300

    правильно указывает пути

--- menu.js
+++ menu.js
@@ -1,5 +1,5 @@
 import menuElement from './menu.json';
-import menuTemplate from '.templates/menu.hbs';
+import menuTemplate from './templates/menu.hbs';
 import './theme.js';
 import './styles.css';
 
@@ -8,7 +8,7 @@ let menu = document.querySelector('.js-menu');
 console.log(menu);
 
 function createMenuCards(array) {
-  const markup = array.map(post => menuTemplate(post)).join('');
+  const markup = array.map(ingredients => menuTemplate(ingredients)).join('');
   menu.insertAdjacentHTML('beforeend', markup);
 }
 
