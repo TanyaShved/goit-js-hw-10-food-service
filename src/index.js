ddc024826ab11d66a64f510615e8784068a2eff5
TanyaShved <tanya.shved.89@gmail.com>
2020-10-22 16:25:35 +0300

    Переустанавливает сборку

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
-import menuElement from './menu.json';
-import menuTemplate from './templates/menu.hbs';
-import './theme.js';
 import './styles.css';
-
-let menu = document.querySelector('.js-menu');
-
-function createMenuCards(array) {
-  const markup = array.map(ingredients => menuTemplate(ingredients)).join('');
-  menu.insertAdjacentHTML('beforeend', markup);
-}
-
-createMenuCards(menuElement);
